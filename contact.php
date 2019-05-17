320921168fffe50f3aaff63df110748be2adee20
Vladislav <kost443562@gmail.com>
2019-05-17 22:50:24 +0300

    contact

--- contact.php
+++ contact.php
@@ -17,30 +17,6 @@
       </nav>
     <a class="btn btn-outline-primary" href="#">Log in</a>
   </div>
-  <div class="container mt-5">
-    <h3 class="mb-5">Our articles</h3>
-    <div class="d-flex flex-wrap">
-    <?php
-      for ($i=0; $i < 6; $i++):
-      ?>
-      <div class="card mb-4 shadow-sm">
-        <div class="card-header">
-            <h4 class="my-0 font-weight-normal">Text</h4>
-          </div>
-          <div class="card-body">
-            <img src="image/<?php echo ($i + 1)?>.jpg" class="img-thumbnail" alt="nature">
-              <ul class="list-unstyled mt-3 mb-4">
-              <li>10 users included</li>
-              <li>2 GB of storage</li>
-              <li>Email support</li>
-              <li>Help center access</li>
-            </ul>
-            <button type="button" class="btn btn-lg btn-block btn-outline-primary">More</button>
-          </div>
-      </div>
-      <?php endfor; ?>
-    </div>
-  </div>
   <footer class="container pt-4 my-md-5 pt-md-5 border-top">
       <div class="row">
         <div class="col-12 col-md">
