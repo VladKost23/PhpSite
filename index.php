5286ec6c587bb7dc4ac613594f39fdf45928049d
Vladislav <kost443562@gmail.com>
2019-05-17 21:58:11 +0300

    start

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<link>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css"></link>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"></link>
+    <title>VladosK</title>
+</head>
+<body>
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-dark border-bottom shadow-sm">
+      <h5 class="my-0 mr-md-auto font-weight-normal text-white">VladosK</h5>
+      <nav class="my-2 my-md-0 mr-md-3">
+        <a class="p-2 text-white" href="#">Home</a>
+        <a class="p-2 text-white" href="#">Support</a>
+        <a class="p-2 text-white" href="#">About Us</a>
+      </nav>
+    <a class="btn btn-outline-primary" href="#">Log in</a>
+  </div>
+  <div class="container mt-5">
+    <h3 class="mb-5">Our articles</h3>
+
+    <?php
+    for ($i=0; $i < 5; $i++):
+    ?>
+    <div class="card mb-4 shadow-sm">
+      <div class="card-header">
+        <h4 class="my-0 font-weight-normal">Free</h4>
+      </div>
+      <div class="card-body">
+        <h1 class="card-title pricing-card-title">$0 <small class="text-muted">/ mo</small></h1>
+        <ul class="list-unstyled mt-3 mb-4">
+          <li>10 users included</li>
+          <li>2 GB of storage</li>
+          <li>Email support</li>
+          <li>Help center access</li>
+        </ul>
+        <button type="button" class="btn btn-lg btn-block btn-outline-primary">Sign up for free</button>
+      </div>
+    </div>
+  <?php endfor; ?>
+  </div>
+
+</body>
+</html>
